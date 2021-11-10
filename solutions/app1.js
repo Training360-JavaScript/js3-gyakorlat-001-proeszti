d2fce7e8354c14d99653185e4b232d27b52958c7
proeszti <kispothajas@gmail.com>
2021-11-10 23:11:16 +0100

    app1 small changes

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,16 +1,16 @@
 'use strict';
-// const removeDuplicatesFromArray = (arr) => {
-//     newArray = [];
-//     arr.forEach((a) => {
-//         if (!newArray.includes(a)) {
-//             newArray.push(a);
-//         }
-//     });
-//     return newArray;
-// }
+const removeDuplicatesFromArray = (arr) => {
+    newArray = [];
+    arr.forEach((a) => {
+        if (!newArray.includes(a)) {
+            newArray.push(a);
+        }
+    });
+    return newArray;
+}
 
-const removeDuplicatesFromArray = arr.reduce((unique, item) =>
-    unique.includes(item) ? unique : [...unique, item], [])
+// const removeDuplicatesFromArray = arr.reduce((unique, item) =>
+//     unique.includes(item) ? unique : [...unique, item], [])
 
 
 
