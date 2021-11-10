4dab65e880e56d7fdb53c8c08cbc317906a8b81d
proeszti <kispothajas@gmail.com>
2021-11-10 23:48:33 +0100

    app2 first format

--- solutions/app2.js
+++ solutions/app2.js
@@ -1,7 +1,13 @@
+const HU = {
+    date(date) {
+        return new Intl.DateTimeFormat('hu-HU').format(date);
+    },
+    currency(money) { return new Intl.NumberFormat('hu-HU', { style: 'currency', currency: 'HUF' }).format(money) },
+    list(arr) { return `${arr[0]}, ${arr[1]} és ${arr[2]}` },
+};
 
 
 
 
 
-
-// export default HU
+export default HU
