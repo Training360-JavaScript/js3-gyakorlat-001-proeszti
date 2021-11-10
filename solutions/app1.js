54c518e93c1eeddacb298610230d2270c44c9525
proeszti <kispothajas@gmail.com>
2021-11-10 23:10:14 +0100

    app1 first try

--- solutions/app1.js
+++ solutions/app1.js
@@ -1,7 +1,19 @@
+'use strict';
+// const removeDuplicatesFromArray = (arr) => {
+//     newArray = [];
+//     arr.forEach((a) => {
+//         if (!newArray.includes(a)) {
+//             newArray.push(a);
+//         }
+//     });
+//     return newArray;
+// }
+
+const removeDuplicatesFromArray = arr.reduce((unique, item) =>
+    unique.includes(item) ? unique : [...unique, item], [])
 
 
 
 
 
-
-// export default removeDuplicatesFromArray
+export default removeDuplicatesFromArray;
